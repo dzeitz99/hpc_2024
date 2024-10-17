2d8e23a26aeb8b4469a6aabf1f7649ec3d0ce508
Daniel Zeitz <dzeitz@eiger-ln001.cscs.ch>
2024-10-17 21:31:45 +0200

    changed script1 to the temperature script

--- exercise_session_04/measured/script1.sh
+++ exercise_session_04/measured/script1.sh
@@ -1,3 +1,37 @@
 #!/bin/bash
 
-echo "some script..."
+output="out.txt"
+
+> $output
+
+total_temp=0
+total_count=0
+
+for input_file in measurements*.txt; do
+    if [[ -f $input_file ]]; then
+        echo "File: $input_file" >> $output
+        echo "TEMPERATURE TIME" >> $output
+        sum_temp=0
+        count=0
+        temp=0
+	time=0
+	while IFS= read -r line
+        do
+            if [[ $line == Temperature:* ]]; then
+                temp=$(echo $line | awk -F '[: ]+' '{print $2}')
+                time=$(echo $line | awk -F '[: ]+' '{print $6}')
+                echo "$temp $time" >> $output
+                sum_temp=$(echo "$sum_temp + $temp" | bc)
+                count=$((count + 1))
+            fi
+        done < "$input_file"
+        total_temp=$(echo "$total_temp + $sum_temp" | bc)
+        total_count=$((total_count + count))
+
+        echo "end of $input_file" >> $output
+    fi
+done
+if [[ $total_count -ne 0 ]]; then
+	avg_temp=$(echo "scale=2; $total_temp / $total_count" | bc)
+	echo "Overall Average Temperature: $avg_temp" >> $output
+fi
