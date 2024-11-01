2e5cc5af1a125ecebcf9840b2da8bd7193a55380
Daniel Zeitz <dzeitz@eiger-ln002.cscs.ch>
2024-11-01 07:13:38 +0100

    added the inverse: Irecv-Isend-Waitall

--- exercise_session_06/ring_and_deadlocks/ring.c
+++ exercise_session_06/ring_and_deadlocks/ring.c
@@ -24,8 +24,9 @@ int main(int argc, char** argv) {
     // Loop over the number of processes
     send_rank = my_rank;
     MPI_Request requests[2];
-    MPI_Isend(&send_rank, 1, MPI_INT, right_rank, 0, MPI_COMM_WORLD, &requests[0]);
-    MPI_Irecv(&recv_rank, 1, MPI_INT, left_rank, 0, MPI_COMM_WORLD, &requests[1]);
+    //MPI_Isend(&send_rank, 1, MPI_INT, right_rank, 0, MPI_COMM_WORLD, &requests[0]);
+    MPI_Irecv(&recv_rank, 1, MPI_INT, left_rank, 0, MPI_COMM_WORLD, &requests[0]);
+    MPI_Isend(&send_rank, 1, MPI_INT, right_rank, 0, MPI_COMM_WORLD, &requests[1]);
     MPI_Waitall(2,requests, MPI_STATUSES_IGNORE);
     my_sum += recv_rank;
     my_rank = recv_rank;
